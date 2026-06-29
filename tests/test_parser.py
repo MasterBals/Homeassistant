41d35cd7115130e488ada2387cdcc5ffaf2c1a03
MasterBals <64681245+MasterBals@users.noreply.github.com>
2026-06-29 21:37:14 +0200

    Cover Chur results table parsing

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -34,6 +34,36 @@ def test_parse_events_prefers_data_order() -> None:
     assert events[0].waste_type == "Papier"
 
 
+def test_parse_events_from_chur_results_table() -> None:
+    streets = [Street(id="18794", name="Ackerbühlstrasse")]
+    html = """
+    <table id="abfuhrplan">
+      <tbody>
+        <tr>
+          <td>Karton</td>
+          <td data-order="2026-07-01T00:00:00+02:00 07:00 Uhr">
+            <a href="/_rte/anlass/7242446">01.07.2026, 07:00 Uhr</a>
+          </td>
+          <td>7242446</td>
+        </tr>
+        <tr>
+          <td>Kompostierbare Abfälle</td>
+          <td data-order="2026-07-22T00:00:00+02:00 07:00 Uhr">
+            <a href="/_rte/anlass/7242509">22.07.2026, 07:00 Uhr</a>
+          </td>
+          <td>7242509</td>
+        </tr>
+      </tbody>
+    </table>
+    """
+    events = parse_events(html, streets, today=date(2026, 6, 29))
+    assert [(event.date, event.waste_type) for event in events] == [
+        (date(2026, 7, 1), "Karton"),
+        (date(2026, 7, 22), "Kompost"),
+    ]
+    assert all(event.street == "Ackerbühlstrasse" for event in events)
+
+
 def test_parse_events_filters_past() -> None:
     streets = [Street(id="12", name="Bahnhofstrasse")]
     html = '<div data-street-id="12">Kehricht Bahnhofstrasse 01.01.2026</div>'
